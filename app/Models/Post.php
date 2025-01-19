c6a13371d0a97d7f9f85ffa1155c9763fe932ad7
Víctor J <maximovj@outlook.com>
2025-01-19 16:09:40 -0600

    * dev-post: Se agrega Post

---
+++ app/Models/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use MoonShine\Laravel\Models\MoonshineUser;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    protected $table = 'posts';
+
+    protected $casts = [
+        'network_social' => 'array',
+    ];
+
+    protected $fillable = [
+        'moonshine_user_id',
+        'cover',
+        'slug',
+        'title',
+        'subtitle',
+        'author',
+        'published',
+        'tags',
+        'network_social',
+        'summary',
+        'header',
+        'content',
+        'footer',
+        'published',
+        'date_published',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(MoonshineUser::class, "moonshine_user_id", "id");
+    }
+
+}
+
