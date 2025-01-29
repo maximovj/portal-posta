e2a84eb658df6fc04561714250fe7ad3385a1236
Víctor J. <maximovj@outlook.com>
2025-01-28 19:50:24 -0600

    * desarrollo : Se modifica vistas y estilos SASS
    // !# Cambios
    - Se modifica vistas y estilos SASS
    - Se agrega media queries en SASS
    - Se agrega nuevos estilos
    - Se modifica el diseño de la vista welcome
    - Se modifica el diseño de la vista de article

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,153 +1,75 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="{!! $article->summary ?? 'Resumen del artículo' !!}">
-    <title>Article</title>
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-    <!-- Botón flotante -->
-    <button id="backToTop" class="btn btn-primary btn-lg rounded-circle">
-        <i class="bi bi-arrow-up"></i>
-    </button>
-    <div class="container">
-        <header id="stickyHeader" class="mb-4 bg-sticky-header p-3 rounded">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-navbar-header" >
-                <div class="container-fluid">
-                  <a class="navbar-brand" href="/">Portal Posta</a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Inicio</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Noticias</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Artículos</a>
-                      </li>
-                    </ul>
-                    <form class="d-flex ms-auto">
-                      <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-                      <button class="btn btn-outline-light" type="submit">Buscar</button>
-                    </form>
-                    <ul class="navbar-nav ms-3">
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Más
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <li><a class="dropdown-item" href="#">Opción 1</a></li>
-                          <li><a class="dropdown-item" href="#">Opción 2</a></li>
-                          <li><a class="dropdown-item" href="#">Opción 3</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </div>
+@extends('layout.base')
+
+@section('page_title')
+Artículo | {{ $article->title ?? ('Artículo No. #' . $article->id) }}
+@endsection
+
+@section('page_description')
+{{ $article->summary ?? 'Resumen del artículo' }}
+@endsection
+
+@section('content_header')
+<div class="d-none d-lg-block shadow-sm p-3 bg-body rounded-bottom">
+    <div class="d-none d-lg-flex article-header">
+        <div>
+            <h1 class="inline fs-3 fw-bold">{!! $article->title ?? ''  !!}</h1>
+            <h2 class="fs-6">{!! $article->subtitle ?? ''  !!}</h2>
+            <span class="inline-block lead fw-bold fs-6">{!! $article->author ?? ''  !!}</span>
+            @can('update', $article)
+                <div class="my-2">
+                    <a 
+                    target="_blank" 
+                    rel="alternate" 
+                    referrerpolicy="origin"
+                    class="btn btn-danger btn-sm"
+                    href="{{ route('moonshine.resource.page', [
+                        'resourceUri' => 'article-resource',
+                        'pageUri' => 'form-page',
+                        'resourceItem' => $article->id,
+                    ]) }}">Modificar mi artículo</a>
                 </div>
-            </nav>
-            <div class="d-flex justify-content-between align-items-center mt-5">
-                <div>
-                    <h1 class="inline fs-3 fw-bold">{!! $article->title ?? ''  !!}</h1>
-                    <h2 class="fs-6">{!! $article->subtitle ?? ''  !!}</h2>
-                    <span class="inline-block lead fw-bold fs-6">{!! $article->author ?? ''  !!}</span>
-                    @can('update', $article)
-                        <div class="my-2">
-                            <a 
-                            role="button" 
-                            class="btn btn-danger btn-sm"
-                            href="{{ route('moonshine.resource.page', [
-                                'resourceUri' => 'article-resource',
-                                'pageUri' => 'form-page',
-                                'resourceItem' => $article->id,
-                            ]) }}">Modificar mi artículo</a>
-                        </div>
-                    @endcan
-                </div>
-                <div>
-                    <p style="font-size:14px;">Publicado en: {{ $article->published_at->format('d/m/Y') }}</p>
-                    @foreach (explode(',', $article->tags) as $tag)
-                        <span class="badge bg-secondary"><i class="bi bi-tag"></i>&nbsp;{{ $tag }}</span>
-                    @endforeach
-                </div>
-            </div>
-        </header>
-        <hr class="mx-0 my-4" />
-        <main>
-            {!! $article->header  !!}
-            {!! $article->content  !!}
-            {!! $article->footer  !!}
-        </main>
-        <hr class="mx-0 my-4 " />
-        <footer class="shadow-sm p-3 mb-5 bg-body rounded">
-            <div class="d-flex justify-content-between align-items-center">
-                <div>
-                    <p class="inline-block lead fw-bold">{!! $article->author ?? $article->moonshine_user->name  ?? 'Desconocido' !!}</p>
-                    <p style="font-size:14px;">Publicado en: {{ $article->published_at->format('d/m/Y') }}</p>
-                    <div>
-                        @foreach (explode(',', $article->tags) as $tag)
-                            <span class="badge bg-secondary"><i class="bi bi-tag"></i>&nbsp;{{ $tag }}</span>
-                        @endforeach
-                    </div>
-                </div>
-                <ul class="list-unstyled grid">
-                    @foreach ($article->network_social as $social )
-                        @if($social['active'])
-                            <li class="g-col-4 ">
-                                <span class="me-1"><i class="bi bi-{{ strtolower($social['title']) }}"></i></span>
-                                <span class="fw-bold fs-6">{{ $social['title'] }}</span> 
-                                <span>:</span> 
-                                <span>{{ $social['value'] }}</span>
-                            </li>
-                        @endif
-                    @endforeach
-                </ul>
-            </div>
-        </footer>
+            @endcan
+        </div>
+        <div>
+            <p style="font-size:14px;">Publicado en: {{ $article->published_at->format('d/m/Y') }}</p>
+            @foreach (explode(',', $article->tags) as $tag)
+                <span class="badge bg-secondary"><i class="bi bi-tag"></i>&nbsp;{{ $tag }}</span>
+            @endforeach
+        </div>
     </div>
-    <script>
-        // Obtener el botón
-        const backToTopButton = document.getElementById('backToTop');
+</div>
+@endsection
 
-        // Mostrar el botón cuando el usuario se desplaza hacia abajo
-        window.addEventListener('scroll', () => {
-        if (window.scrollY > 300) { // Si ha bajado más de 300px
-            backToTopButton.style.display = 'block';
-        } else {
-            backToTopButton.style.display = 'none';
-        }
-        });
+@section('content_main')
+{!! $article->header  !!}
+{!! $article->content  !!}
+{!! $article->footer  !!}
+@endsection
 
-        // Volver al inicio al hacer clic en el botón
-        backToTopButton.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth' // Animación suave
-        });
-        });
-
-        document.addEventListener('DOMContentLoaded', () => {
-            const header = document.getElementById('stickyHeader');
-            const breakpoint = 768; // Ancho mínimo para aplicar sticky
-            const stickyOffset = header.offsetTop; // Posición original del header
-
-            window.addEventListener('scroll', () => {
-                if (window.innerWidth >= breakpoint) {
-                    if (window.pageYOffset > stickyOffset) {
-                        header.classList.add('shadow', 'sticky-lg-top');
-                    } else {
-                        header.classList.remove('shadow', 'sticky-lg-top');
-                    }
-                } else {
-                    header.classList.remove('shadow', 'sticky-lg-top'); // Quitar sticky en pantallas pequeñas
-                }
-            });
-        });
-    </script>
-</body>
-</html>
+@section('content_footer')
+<div class="shadow-sm p-3 mb-5 bg-body rounded">
+    <div class="d-flex justify-content-between align-items-center">
+        <div>
+            <p class="inline-block lead fw-bold">{!! $article->author ?? $article->moonshine_user->name ?? 'Desconocido' !!}</p>
+            <p style="font-size:14px;">Publicado en: {{ $article->published_at->format('d/m/Y') }}</p>
+            <div>
+                @foreach (explode(',', $article->tags) as $tag)
+                <span class="badge bg-secondary"><i class="bi bi-tag"></i>&nbsp;{{ $tag }}</span>
+                @endforeach
+            </div>
+        </div>
+        <ul class="list-unstyled grid">
+            @foreach ($article->network_social as $social )
+            @if($social['active'])
+            <li class="g-col-4 ">
+                <span class="me-1"><i class="bi bi-{{ strtolower($social['title']) }}"></i></span>
+                <span class="fw-bold fs-6">{{ $social['title'] }}</span>
+                <span>:</span>
+                <span>{{ $social['value'] }}</span>
+            </li>
+            @endif
+            @endforeach
+        </ul>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,107 @@
 @extends('layout.base')
 
-@section('main_content')
-@foreach ($articles as $article)
-<div class="d-flex flex-row justify-content-between align-items-center mt-5">
-    <div>
-        <div>
-            <h1 class="inline fs-3 fw-bold">{!! $article->title ?? '' !!}</h1>
-            <h2 class="fs-6">{!! $article->subtitle ?? '' !!}</h2>
-            <span class="inline-block lead fw-bold fs-6">{!! $article->author ?? '' !!}</span>
+@section('content_main')
+<div id="carouselArticles" class="carousel slide mb-5" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        @foreach ($articles as $index => $article)
+        <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+            <div class="card shadow-sm">
+                @if($article->cover) 
+                <img src="{{ asset("storage/$article->cover ") }}" class="card-img-top" alt="Imagen del artículo" style="height: 360px; object-fit: cover;">
+                @else
+                <img src="{{ asset("storage/posts/er4MgUJVMWPiSmpslmoJ8BegWaCO7emq5HCGXG0p.jpg") }}" class="card-img-top" alt="Imagen del artículo" style="height: 360px; object-fit: cover;">
+                @endif
+                <div class="card-body">
+                    <h1 class="fs-3 fw-bold">{!! $article->title ?? '' !!}</h1>
+                    <h2 class="fs-6 text-muted">{!! $article->subtitle ?? '' !!}</h2>
+                    <p class="fs-6 text-secondary">{!! $article->author ?? '' !!}</p>
+                    <p class="fs-6 text-muted">Publicado en: {{ $article->published_at->format('d/m/Y') }}</p>
+                    <div class="d-flex flex-wrap">
+                        @foreach (explode(',', $article->tags) as $tag)
+                            <span class="badge bg-secondary me-1"><i class="bi bi-tag"></i>&nbsp;{{ $tag }}</span>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="card-footer d-flex justify-content-center">
+                    <a role="button" href="{{ route('portal.posta.article', ['article' => $article]) }}" class="btn btn-outline-success rounded-pill" data-bs-slide-to="false">
+                        Seguir leyendo <i class="bi bi-arrow-right-circle ms-2"></i>
+                    </a>
+                </div>
+            </div>
         </div>
-        <div>
-            <p style="font-size:14px;">Publicado en: {{ $article->published_at->format('d/m/Y') }}</p>
+        @endforeach
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselArticles" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselArticles" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+@foreach ($articles as $article)
+<div class="card shadow-sm border-0 mb-4" style="border-radius: 10px;">
+    
+    @if($article->cover) 
+    <img loading="lazy" src="{{ asset("storage/$article->cover ") }}" class="card-img-top" alt="Imagen del artículo" style="height: 420px; object-fit: cover; opacity: 1;">
+    @else
+    <img loading="lazy" src="{{ asset("storage/posts/er4MgUJVMWPiSmpslmoJ8BegWaCO7emq5HCGXG0p.jpg") }}" class="card-img-top" alt="Imagen del artículo" style="height: 420px; object-fit: cover; opacity: 1;">
+    @endif
+
+    <div class="card-body">
+        <h1 class="fs-4 fw-bold text-dark">{!! $article->title ?? '' !!}</h1>
+        <h2 class="fs-6 text-muted">{!! $article->subtitle ?? '' !!}</h2>
+        <p class="fs-6 text-muted">{!! $article->author ?? '' !!} | {{ $article->published_at->format('d/m/Y') }}</p>
+
+        <div class="d-flex flex-wrap">
             @foreach (explode(',', $article->tags) as $tag)
-            <span class="badge bg-secondary"><i class="bi bi-tag"></i>&nbsp;{{ $tag }}</span>
+                <span class="badge bg-secondary me-2 mb-2">{{ $tag }}</span>
             @endforeach
         </div>
+
+        <!-- Desplegar contenido con animación fade-in -->
+        <div class="collapse mt-3 show" id="articleHeader{{ $article->id }}">
+            <div class="card-body bg-light rounded-3">
+                {!! $article->header !!}
+            </div>
+        </div>
+        
+        <button class="btn btn-link text-primary mt-3 text-decoration-none" type="button" data-btn-toggle="item-article-header" data-bs-toggle="collapse" data-bs-target="#articleHeader{{ $article->id }}" aria-expanded="false" aria-controls="articleHeader{{ $article->id }}">
+            <span id="article-text-header-span">Ocultar introducción</span> <i class="bi bi-chevron-up ms-2"></i>
+        </button>
+    </div>
+    
+    <div class="card-footer d-grid gap-2 d-md-flex justify-content-md-end bg-transparent">
+        <a href="{{ route('portal.posta.article', ['article' => $article]) }}" class="btn btn-outline-dark rounded-pill ">
+            Seguir leyendo <i class="bi bi-arrow-right-circle ms-2"></i>
+        </a>
     </div>
 </div>
-<hr class="mx-0 my-4" />
-<main>
-    {!! $article->header !!}
-    <a href="{{ route('portal.posta.article', ['article' => $article]) }}">Leer más</a>
-</main>
 @endforeach
 
 {{ $articles->links('pagination::bootstrap-5') }}
 @endsection
+
+
+@push('custom_script')
+<script>
+    document.querySelectorAll('button[data-btn-toggle="item-article-header"').forEach(button => {
+        button.addEventListener('click', () => {
+            const isExpanded = button.getAttribute('aria-expanded') === 'true';
+            const span = button.querySelector('#article-text-header-span');
+            const icon = button.querySelector('i');
+
+            // Cambiar texto e ícono según el estado
+            if (isExpanded) {
+                span.textContent = 'Ocultar introducción';
+                icon.className = 'bi bi-chevron-up ms-2';
+            } else {
+                span.textContent = 'Mostrar introducción';
+                icon.className = 'bi bi-chevron-down ms-2';
+            }
+        });
+    });
+</script>
+@endpush
